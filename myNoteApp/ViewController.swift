1a1abc9a1caea3757ecaa42d90608a66e5f85b67
OlgaVovolka <o.vovolka@outlook.com>
2019-05-23 22:19:46 +0300

    Initial Commit

---
+++ myNoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myNoteApp
+//
+//  Created by ДаблЧери on 5/23/19.
+//  Copyright © 2019 CAT&GIRL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
